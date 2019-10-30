1b6f6e4240d6650f13045cc9ffcf25424fc5eb37
Matty Courtney <matty@mattycourtney.com>
2019-10-30 18:23:11 +1100

    Added support for multiple personas

--- api-client.rb
+++ api-client.rb
@@ -3,15 +3,21 @@ require 'httparty'
 
 class SinatraApp
   include HTTParty
-  logger ::Logger.new(STDOUT)
+  logger ::Logger.new(STDOUT), :info, :logstash
   base_uri 'sinatra-app:4567'
 end
 
 logger = Logger.new(STDOUT)
 
+regular_personas = %w(richard erlich bighead gilfoyle dinesh)
+special_personas = %w(monica jared gavin jian-yang laurie russ)
+
 loop do
+  # Select a random persona, grabbing one from the 'special' personas if the loop executes within the first 10 seconds of a minute
+  persona = Time.now.sec >= 10 ? regular_personas.sample : special_personas.sample 
   fortune = `/usr/games/fortune fortunes literature`
-  SinatraApp.post('/post', {body: {msg: fortune}})
-  logger.info "Sleeping for 3 seconds..."
-  sleep 3
+  SinatraApp.post('/post', {body: {message: fortune, persona: persona}})
+  seconds = rand(1..10)
+  logger.info "Sleeping for #{seconds} seconds..."
+  sleep seconds
 end
