cb657623acc2cde85adb1dee61a196ac371a0421
Matty Courtney <matty@mattycourtney.com>
2019-11-08 13:33:52 +1100

    Rotate through personas instead of picking one at random

--- api-client.rb
+++ api-client.rb
@@ -9,12 +9,12 @@ end
 
 logger = Logger.new(STDOUT)
 
-regular_personas = %w(richard erlich bighead gilfoyle dinesh)
-special_personas = %w(monica jared gavin jian-yang laurie russ)
+regular_personas = %w(bighead richard erlich gilfoyle dinesh jared)
+special_personas = %w(russ monica gavin jian-yang laurie)
 
 loop do
   # Select a random persona, grabbing one from the 'special' personas if the loop executes within the first 10 seconds of a minute
-  persona = Time.now.sec >= 10 ? regular_personas.sample : special_personas.sample 
+  persona = Time.now.sec >= 10 ? regular_personas.rotate!.first : special_personas.rotate!.first 
   fortune = `/usr/games/fortune -s fortunes`
   SinatraApp.post('/post', {body: {message: fortune, persona: persona}})
   seconds = rand(3..10)
